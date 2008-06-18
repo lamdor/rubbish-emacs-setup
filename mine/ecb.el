4cd1bbdb0fcc49a15b53e1634ef30c9f5202611d
Luke Amdor <luke@absoluterubbish.net>
2008-06-18 18:24:53 -0500

    Auto-activating ecb

--- mine/ecb.el
+++ mine/ecb.el
@@ -2,20 +2,14 @@
 (add-path "site-lisp/ecb-2.32/")
 (require 'ecb-autoloads)
 
-(setq ecb-source-path (quote ("~/code/learning/bank" "~/code" )))
-
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ '(ecb-source-path (quote ("~/code/learning/bank" "~/code" )))
  '(ecb-options-version "2.32")
  '(ecb-wget-setup (quote cons))
  '(paren-match-face (quote paren-face-match-light))
- '(paren-sexp-mode t))
+ '(paren-sexp-mode t)
+ '(ecb-auto-activate t)
+)
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+
  )

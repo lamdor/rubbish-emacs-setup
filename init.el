56eb3c60111c44adc214c0f93a4ac1af96c56c29
Luke Amdor <luke@absoluterubbish.net>
2008-06-24 20:49:45 -0500

    Added start up time message

--- init.el
+++ init.el
@@ -5,9 +5,13 @@
 (defun add-path (p)
   (add-to-list 'load-path (concat emacs-root p)))
 
+(defvar *emacs-load-start* (current-time))
+
 (add-path "mine")
 (load "dependencies.el")
 (load "customizations.el")
 (load "ruby.el")
 (load "ecb.el")
 (load "erlang.el")
+
+(message "My .emacs loaded in %ds." (destructuring-bind (hi lo ms) (current-time) (- (+ hi lo) (+ (first *emacs-load-start*) (second *emacs-load-start*)))))

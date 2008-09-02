af5e5d02dad799dddf5a01a0c0c8f484a1c4ff0b
Luke Amdor <luke@absoluterubbish.net>
2008-09-01 20:59:15 -0500

    added nxml-mode

--- mine/dependencies.el
+++ mine/dependencies.el
@@ -31,6 +31,12 @@
 (yas/initialize)
 (yas/load-directory (concat emacs-root "site-lisp/yasnippet-0.5.4/snippets/"))
 
+;; nxml-mode
+(load-file (concat emacs-root "site-lisp/nxml-mode-20041004/rng-auto.el"))
+(setq auto-mode-alist
+      (cons '("\\.\\(xml\\|xsl\\|rng\\|xhtml\\)\\'" . nxml-mode)
+	    auto-mode-alist))
+
 ;; Misc Files
 (add-path "el/")
 (require 'find-recursive)

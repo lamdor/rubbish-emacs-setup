8bae5fe0249ea755d4b422185bc4684179f47ac0
Luke Amdor <luke@absoluterubbish.net>
2008-09-14 16:15:19 -0500

    Added hook for syntactic goodness

--- mine/java.el
+++ mine/java.el
@@ -1,5 +1,12 @@
 ;; Settings for Java
 
+(add-hook 'c-mode-common-hook
+	  (lambda ()
+	    (c-toggle-electric-state 1)
+	    (c-toggle-auto-newline 1)
+	    (c-toggle-syntactic-indentation 1)))
+
+
 (autoload 'mvn-keys "mvn" "Sets Maven Keys")
 
 (add-hook 'java-mode-hook (lambda () (mvn-keys)))

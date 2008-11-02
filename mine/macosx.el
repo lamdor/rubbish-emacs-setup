815e79a19cafc464abed0553faac74ae49602805
Luke Amdor <luke@absoluterubbish.net>
2008-11-02 07:58:56 -0600

    Added mine/macosx.el for mac specific config

--- mine/macosx.el
+++ mine/macosx.el
@@ -1,4 +1,4 @@
 ;; Needed for JDEE
-;;(setenv "JAVA_HOME" "/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/")
-(setenv "JAVA_HOME" "/Library/Java/Home")
+(setenv "JAVA_HOME" "/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/")
+;;(setenv "JAVA_HOME" "/Library/Java/Home")
 (setenv "JAVACMD" "/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home/bin/java")

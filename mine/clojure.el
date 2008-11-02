250aa4f7c1d4ae403de0fcba3b0517e3b0e28b71
Luke Amdor <luke@absoluterubbish.net>
2008-11-02 08:08:55 -0600

    Added paredit structured editting

--- mine/clojure.el
+++ mine/clojure.el
@@ -1,4 +1,9 @@
 ;; Clojure
 
+(autoload 'paredit-mode "paredit" "Minore module for psuedo-structually editting lisp code." t)
+
 (add-path "site-lisp/clojure-mode")
 (require 'clojure-auto)
+(require 'clojure-paredit)
+
+(add-hook clojure-mode-hook (lambda () (paredit-mode t)))

2a46d98c8361f5ddd7e800077cb023d53c21da8f
Luke Amdor <luke@absoluterubbish.net>
2008-11-22 12:15:52 -0600

    Added global-key-set for magit-status

--- mine/customizations.el
+++ mine/customizations.el
@@ -8,6 +8,7 @@
 (global-set-key "\r" 'newline-and-indent)
 (global-set-key (kbd "C-c C-a") 'autotest-switch)
 (global-set-key (kbd "C-k") 'kill-to-end-or-join)
+(global-set-key (kbd "C-c C-m") 'magit-status)
 
 (global-unset-key "\C-z")
 

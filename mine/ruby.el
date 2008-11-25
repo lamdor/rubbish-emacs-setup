0278b9eac2f7905591306b0a03f2ca7e3733c92c
Luke Amdor <luke@absoluterubbish.net>
2008-11-24 21:36:55 -0600

    Reorganized ruby.el

--- mine/ruby.el
+++ mine/ruby.el
@@ -1,33 +1,42 @@
+;; Ruby Dependencies
+
+(add-path "site-lisp/ri-emacs/")
+
 (require 'ruby-electric)
 (require 'rdebug)
 (require 'ruby-style)
 (require 'autotest)
+(autoload 'ruby-mode "ruby-mode" "Major mode for editing Ruby code" t)
+(autoload 'run-ruby "inf-ruby" "Runs an inferior ruby process")
+(autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode")
+(autoload 'ri "ri" "lookup Ruby documentation" t)
 
-;; Ruby Setup
+;; File Types
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-(autoload 'ruby-mode "ruby-mode" "Major mode for editing Ruby code" t)
+
+;; Hooks
 (add-hook 'ruby-mode-hook (lambda () (ruby-electric-mode t)))
-
-;; inferior ruby support
-(autoload 'run-ruby "inf-ruby" "Runs an inferior ruby process")
-(autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode")
 (add-hook 'ruby-mode-hook '(lambda() (inf-ruby-keys)))
-
 (add-hook 'ruby-mode-hook '(lambda() (local-set-key "\r" 'ruby-reindent-then-newline-and-indent)))
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+	     (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
+		 (flymake-mode))
+	     ))
 
+;; Ri setup
+(setq ri-repl-executable "ri_repl")
+
+;; Misc
 (defun run-jruby ()
   (interactive)
     (run-ruby "/usr/local/jruby/current/bin/jruby -S irb --inf-ruby-mode"))
 
-(add-path "site-lisp/ri-emacs/")
-(setq ri-repl-executable "ri_repl")
-(autoload 'ri "ri" "lookup Ruby documentation" t)
 
-;; Flymake Ruby
+;; Flymake Flymake
 (require 'flymake)
-;; I don't like the default colors :)
 (set-face-background 'flymake-errline "red4")
 (set-face-background 'flymake-warnline "dark slate blue")
 
@@ -42,13 +51,5 @@
 
 (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
 (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-
 (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
 
-(add-hook 'ruby-mode-hook
-          '(lambda ()
-
-	     ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
-	     (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
-		 (flymake-mode))
-	     ))

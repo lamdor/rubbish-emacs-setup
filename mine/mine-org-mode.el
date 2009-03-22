ebf47d3897df7d626cd1ac895f924e2bb17f04a2
Luke Amdor <luke@absoluterubbish.net>
2009-03-22 08:35:46 -0500

    Step one in getting org-mode setup finished

--- mine/mine-org-mode.el
+++ mine/mine-org-mode.el
@@ -6,4 +6,15 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 
+(setq org-hide-leading-stars t)
+(setq org-enforce-todo-dependencies t)
+(setq org-agenda-dim-blocked-tasks t)
+(setq org-log-into-drawer "LOGBOOK")
+(setq org-log-done 'time)
+
+(setq org-todo-keywords 
+      '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)"))) 
+(setq org-link-abbrev-alist
+      '(("google" . "http://www.google.com/search?q=%s")))
+
 (provide 'mine-org-mode)

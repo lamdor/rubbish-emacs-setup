70b507a74dc285db3f42110801f4ce083fda3edc
Luke Amdor <luke@absoluterubbish.net>
2009-04-05 17:15:02 -0500

    Added a command to bring up a pop up frame for inputing into remember mode

--- mine/mine-org-mode.el
+++ mine/mine-org-mode.el
@@ -86,4 +86,28 @@
 (global-set-key (kbd "C-c g j") 'gtd-jump)
 (global-set-key (kbd "C-c g m") 'gtd-someday-maybe)
 
+
+;; for a popup window for remember mode
+(defadvice remember-finalize (after delete-remember-frame activate)
+  "Advise remember-finalize to close the frame if it is the remember frame"
+  (if (equal "remember" (frame-parameter nil 'name))
+      (delete-frame)))
+
+(defadvice remember-destroy (after delete-remember-frame activate)
+  "Advise remember-destroy to close the frame if it is the rememeber frame"
+  (if (equal "remember" (frame-parameter nil 'name))
+      (delete-frame)))
+
+;; make the frame contain a single window. by default org-remember
+;; splits the window.
+(add-hook 'remember-mode-hook
+          'delete-other-windows)
+
+(defun make-remember-frame ()
+  "Create a new frame and run org-remember."
+  (interactive)
+  (make-frame '((name . "remember") (width . 80) (height . 10)))
+  (select-frame-by-name "remember")
+  (org-remember))
+
 (provide 'mine-org-mode)

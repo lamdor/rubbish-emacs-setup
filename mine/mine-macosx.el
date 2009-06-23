e225278d9d2bfbd65a9007586cfa4354953f034f
Luke Amdor <luke@absoluterubbish.net>
2009-06-23 10:16:09 -0500

    Setting default font

--- mine/mine-macosx.el
+++ mine/mine-macosx.el
@@ -4,6 +4,7 @@
 (setenv "JAVACMD" "/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home/bin/java")
 
 (setq ns-command-modifier (quote meta))
+(set-face-font 'default "-apple-Monaco-medium-normal-normal-Regular-14-*-*-*-*-*-fontset-startup")
 (if (functionp 'ns-set-background-alpha)
     (ns-set-background-alpha 0.9))
 

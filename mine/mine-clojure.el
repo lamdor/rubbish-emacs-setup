78d6f5d3e2b6f28f2d2557ba05b2b9de5422f550
Luke Amdor <luke@absoluterubbish.net>
2009-07-22 09:01:00 -0500

    Added clojure-project defun to help start up slime with correct dependencies

--- mine/mine-clojure.el
+++ mine/mine-clojure.el
@@ -1,6 +1,6 @@
 ;; Clojure
 
-(autoload 'paredit-mode "paredit" "Minore module for psuedo-structually editting lisp code." t)
+(autoload 'paredit-mode "paredit" "Minor module for psuedo-structually editting lisp code." t)
 
 (add-path "site-lisp/clojure-mode")
 (autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
@@ -19,6 +19,36 @@
 	    "~/.m2/repository/ant/ant/1.6.5/ant-1.6.5.jar"
 	    (concat emacs-root "mine/clojure/")))
 (require 'swank-clojure-autoload)
+
+;; taken from http://github.com/technomancy/emacs-starter-kit/blob/a46328f2ad0cb437bf22aaf42f4f25f53126102f/starter-kit-lisp.el#L104
+(defun clojure-project (path)
+  "Setup classpaths for a clojure project and starts a new SLIME session.
+
+Kills existing SLIME session, if any."
+  (interactive (list
+                (ido-read-directory-name
+                 "Project root: "
+                 (locate-dominating-file default-directory "pom.xml"))))
+  (when (get-buffer "*inferior-lisp*")
+    (kill-buffer "*inferior-lisp*"))
+  (add-to-list 'swank-clojure-extra-vm-args
+               (format "-Dclojure.compile.path=%s"
+                       (expand-file-name "target/classes/" path)))
+  (setq swank-clojure-binary nil
+        swank-clojure-jar-path (expand-file-name "target/dependency/" path)
+        swank-clojure-extra-classpaths
+        (append (mapcar (lambda (d) (expand-file-name d path))
+                        '("src/" "target/classes/" "test/"))
+                (let ((lib (expand-file-name "lib" path)))
+                  (if (file-exists-p lib)
+                      (directory-files lib t ".jar$"))))
+        slime-lisp-implementations
+        (cons `(clojure ,(swank-clojure-cmd) :init swank-clojure-init)
+              (remove-if #'(lambda (x) (eq (car x) 'clojure))
+                         slime-lisp-implementations)))
+  (save-window-excursion
+    (slime)))
+
 
 ;; clojure-test-mode
 (autoload 'clojure-test-mode "clojure-test-mode" "Clojure test mode" t)

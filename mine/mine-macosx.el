a37867951446d7aec88569d8c77f741a3e55113b
Luke Amdor <luke@absoluterubbish.net>
2009-07-21 22:46:37 -0500

    Figure out how to do transparency the correct way

--- mine/mine-macosx.el
+++ mine/mine-macosx.el
@@ -5,8 +5,18 @@
 
 (setq ns-command-modifier (quote meta))
 (set-frame-font "Monaco-14")
-(if (functionp 'ns-set-background-alpha)
-    (ns-set-background-alpha 0.9))
+
+(set-frame-parameter (selected-frame) 'alpha '(90 80))
+(add-to-list 'default-frame-alist '(alpha 90 80))
+
+(defun toggle-transparency ()
+   (interactive)
+   (if (/=
+        (cadr (find 'alpha (frame-parameters nil) :key #'car))
+        100)
+       (set-frame-parameter nil 'alpha '(100 100))
+     (set-frame-parameter nil 'alpha '(90 80))))
+
 (if (functionp 'scroll-bar-mode)
     (scroll-bar-mode -1))
 

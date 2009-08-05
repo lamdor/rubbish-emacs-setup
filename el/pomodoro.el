5a49f9045fd60793a99418db7eef855b06974369
Luke Amdor <luke@absoluterubbish.net>
2009-08-05 00:23:33 -0500

    Added display of pomdoro timer to global mode line

--- el/pomodoro.el
+++ el/pomodoro.el
@@ -1,28 +1,72 @@
 (require 'timer)
 
-(defvar pomodoro-current-timer)
+(defvar pomodoro-current-timer nil)
 
-(defun pomodoro-finished ()
+(defvar pomodoro-duration-seconds (* 25 60))
+
+(defun pomodoro-format-time-difference (seconds)
+  (let ((absolute-seconds (abs seconds)))
+    (format "%s%02d:%02d" (if (< 0 seconds) "+" "-")
+            (/ absolute-seconds 60)
+            (% absolute-seconds 60))))
+
+(defvar pomodoro-display-string nil)
+(defun pomodoro-display-update ()
+  (setq pomodoro-display-string
+   (and pomodoro-current-timer
+       (format "Pomodoro: %s"
+               (pomodoro-format-time-difference
+                (timer-until pomodoro-current-timer (current-time)))))))
+
+(defvar pomodoro-display-timer nil)
+(define-minor-mode pomodoro-display-mode
+  :global t
+  (or global-mode-string (setq global-mode-string '("")))
+  (or pomodoro-display-timer (cancel-timer pomodoro-display-timer))
+  (if pomodoro-display-mode
+      (progn
+        (or (memq 'pomodoro-display-string global-mode-string)
+            (setq global-mode-string
+                  (append global-mode-string '(pomodoro-display-string))))
+        (setq pomodoro-display-timer
+              (run-at-time t 1
+                           'pomodoro-display-update)))
+    (and pomodoro-display-timer
+         (cancel-timer pomodoro-display-timer))))
+
+(defun pomodoro-start-display ()
+  (pomodoro-display-mode t))
+
+(setq mode-line-format nil)
+
+(defun pomodoro-display (msg)
   (let ((pomdoro-buffer (get-buffer-create "*Pomodoro*")))
     (switch-to-buffer pomdoro-buffer)
     (toggle-read-only -1)
     (erase-buffer)
-    (insert "Pomodoro Finished")
+    (insert msg)
     (toggle-read-only t)))
 
-;; (timer-until pomodoro-current-timer (current-time))
+(defun pomodoro-finished ()
+  (cancel-timer pomodoro-current-timer)
+  (setq pomodoro-current-timer nil)
+  (pomodoro-display "Pomodoro Finished"))
 
 (defun pomodoro-start ()
   "Start a pomodoro timer"
   (interactive)
   (setq pomodoro-current-timer
-        (run-at-time "5 seconds" nil 'pomodoro-finished))
+        (run-at-time (time-add (current-time) (seconds-to-time pomodoro-duration-seconds))
+                     nil
+                     'pomodoro-finished))
+  (pomodoro-start-display)
   (message "Pomodoro started"))
 
 (defun pomodoro-void ()
   "Stops the current pomodoro timer"
   (interactive)
   (cancel-timer pomodoro-current-timer)
+  (setq pomodoro-current-timer nil)
   (message "Pomodoro voided"))
 
 (provide 'pomodoro)

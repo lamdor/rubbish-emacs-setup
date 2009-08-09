ddd0b866256158f6ecb3d6cfebf39f9d5c964423
Luke Amdor <luke@absoluterubbish.net>
2009-08-09 09:10:33 -0500

    Added pending link helper

--- mine/mine-org-mode.el
+++ mine/mine-org-mode.el
@@ -30,6 +30,7 @@
 ;; link configuration
 (setq org-link-abbrev-alist
       '(("google" . "http://www.google.com/search?q=%s")
+        ("pending" . "file:///Users/luke/Desktop/pending/%s")
         ("bcredmine" . "https://redmine.bigcreek.com/issues/show/%s")))
 
 ;; refiling configuration

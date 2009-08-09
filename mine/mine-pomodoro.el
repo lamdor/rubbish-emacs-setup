efb9631bd04c30b47718e3161a2b037f1f244c59
Luke Amdor <luke@absoluterubbish.net>
2009-08-09 09:13:35 -0500

    Added keybindings for starting and voiding pomodoros

--- mine/mine-pomodoro.el
+++ mine/mine-pomodoro.el
@@ -1,4 +1,7 @@
 (add-hook 'pomodoro-finished-hook '(lambda ()
                                      (growl-message "Pomodoro Finished" 2)))
 
+(global-set-key (kbd "C-c p s") 'pomodoro-start)
+(global-set-key (kbd "C-c p v") 'pomodoro-void)
+
 (provide 'mine-pomodoro)

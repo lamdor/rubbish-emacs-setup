e5abb78a15c876e9791b9b1fa9ae7f7654c811f2
Luke Amdor <luke@absoluterubbish.net>
2009-08-10 18:21:25 -0500

    Created mine-* display functions and moved them to mine-pretty

--- mine/mine-macosx.el
+++ mine/mine-macosx.el
@@ -4,20 +4,5 @@
 (setenv "JAVACMD" "/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home/bin/java")
 
 (setq ns-command-modifier (quote meta))
-(set-frame-font "Monaco-14")
-
-(set-frame-parameter (selected-frame) 'alpha '(90 80))
-(add-to-list 'default-frame-alist '(alpha 90 80))
-
-(defun toggle-transparency ()
-   (interactive)
-   (if (/=
-        (cadr (find 'alpha (frame-parameters nil) :key #'car))
-        100)
-       (set-frame-parameter nil 'alpha '(100 100))
-     (set-frame-parameter nil 'alpha '(90 80))))
-
-(if (functionp 'scroll-bar-mode)
-    (scroll-bar-mode -1))
 
 (provide 'mine-macosx)
--- mine/mine-pretty.el
+++ mine/mine-pretty.el
@@ -10,4 +10,48 @@
 (require 'highlight-parentheses)
 (highlight-parentheses-mode t)
 
+;; display settings
+(defun mine-use-normal-font ()
+  (interactive)
+  (set-frame-parameter (selected-frame) 'font "-apple-Monaco-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+  (add-to-list 'default-frame-alist '(font . "-apple-Monaco-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")))
+
+(defun mine-use-big-font ()
+  (interactive)
+  (set-frame-parameter (selected-frame) 'font "-apple-Monaco-medium-normal-normal-*-20-*-*-*-m-0-iso10646-1")
+  (add-to-list 'default-frame-alist '(font . "-apple-Monaco-medium-normal-normal-*-20-*-*-*-m-0-iso10646-1")))
+
+(defun mine-use-transparency ()
+  (interactive)
+  (set-frame-parameter (selected-frame) 'alpha '(90 80))
+  (add-to-list 'default-frame-alist '(alpha 90 80)))
+
+(defun mine-use-no-transparency ()
+  (interactive)
+  (set-frame-parameter (selected-frame) 'alpha '(100 100))
+  (add-to-list 'default-frame-alist '(alpha 100 100)))
+
+(defun mine-toggle-transparency ()
+  (interactive)
+  (if (/=
+       (cadr (find 'alpha (frame-parameters nil) :key #'car))
+       100)
+      (mine-use-transparency)
+    (mine-use-no-transparency)))
+
+(if (functionp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+
+(defun mine-normal-display ()
+  (interactive)
+  (mine-use-normal-font)
+  (mine-use-transparency))
+
+(defun mine-pair-display ()
+  (interactive)
+  (mine-use-big-font)
+  (mine-use-no-transparency))
+
+(mine-normal-display)
+
 (provide 'mine-pretty)

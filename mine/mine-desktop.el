0e0cac98486bba2d13c1cfc28345591f18962cdc
Luke Amdor <luke@absoluterubbish.net>
2009-08-17 13:02:34 -0500

    Automatically saving desktop every 5 minutes

--- mine/mine-desktop.el
+++ mine/mine-desktop.el
@@ -1,4 +1,10 @@
 (setq desktop-path (list emacs-root))
 (desktop-save-mode t)
 
+(defun mine-save-desktop ()
+  (interactive)
+  (desktop-save (car desktop-path) nil))
+
+(run-at-time t 300 'mine-save-desktop)
+
 (provide 'mine-desktop)

b5d68844dfe3d2b0453a8413de6f1048a76ddb49
Luke Amdor <luke@absoluterubbish.net>
2009-09-14 11:24:28 -0500

    Cleaning up mine-scala

--- mine/mine-scala.el
+++ mine/mine-scala.el
@@ -2,20 +2,12 @@
 (add-path "site-lisp/scala")
 (require 'scala-mode-auto)
 
-(autoload 'scala-mode-feature-install
-  "scala-mode-feature" "Add features for scala-mode")
-(autoload 'scala-mode-feature-electric-install
-  "scala-mode-feature-electric" "Scala Electric Mode")
-
-(setq scala-interpreter "/usr/local/scala/current/bin/scala")
+(setq scala-interpreter "/usr/local/bin/scala")
 
 (add-hook 'scala-mode-hook
 	  '(lambda ()
-	     (scala-mode-feature-install)
-	     (scala-mode-feature-electric-install)
 	     (scala-electric-mode t)
-	     (local-set-key (kbd "RET") 'newline-and-indent)
-	     (local-set-key (kbd "C-c C-z") 'scala-run-scala)))
+             (c-subword-mode t)))
 
 (provide 'mine-scala)
 

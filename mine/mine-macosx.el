1a10e5efd2c241c681485465366e9069cfded055
Luke Amdor <luke@absoluterubbish.net>
2009-09-14 11:30:37 -0500

    Fixing JAVA_HOME and JAVA_ENV env vars by not setting them

--- mine/mine-macosx.el
+++ mine/mine-macosx.el
@@ -1,8 +1,3 @@
-;; Needed for JDEE
-(setenv "JAVA_HOME" "/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/")
-;;(setenv "JAVA_HOME" "/Library/Java/Home")
-(setenv "JAVACMD" "/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home/bin/java")
-
 (setq ns-command-modifier (quote meta))
 
 (provide 'mine-macosx)

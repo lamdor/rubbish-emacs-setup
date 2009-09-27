e2aab7d07d4691e3039c3d1f1711fc233fbb7f91
Luke Amdor <luke@absoluterubbish.net>
2009-09-26 20:33:26 -0500

    Fixed clojure jar path

--- mine/mine-slime.el
+++ mine/mine-slime.el
@@ -9,11 +9,11 @@
 (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode t)))
 
 (add-path "site-lisp/swank-clojure")
-(setq swank-clojure-jar-path "/usr/local/clojure/current/clojure-1.0.0.jar") ; needed for swank-clojure to be happy
+(setq swank-clojure-jar-path "/usr/local/Cellar/clojure/1.0.0/clojure-1.0.0.jar") ; needed for swank-clojure to be happy
 (require 'swank-clojure-autoload)
 
 (defmacro mine-swank-clojure-implementation (name &optional extra-classpath java-path clojure-jar extra-vm-args)
-  `(let ((swank-clojure-jar-path ,(or clojure-jar "/usr/local/clojure/current/clojure-1.0.0.jar"))
+  `(let ((swank-clojure-jar-path ,(or clojure-jar "/usr/local/Cellar/clojure/1.0.0/clojure-1.0.0.jar"))
          (swank-clojure-extra-classpaths (cons (concat emacs-root "mine/clojure/") ,(or extra-classpath `(list "~/code/watch/clojure-contrib/src/"))))
          (swank-clojure-java-path ,(or java-path "java"))
          (swank-clojure-extra-vm-args ,extra-vm-args))

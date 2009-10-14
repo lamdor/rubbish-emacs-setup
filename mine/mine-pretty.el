7282aa8eb2163c886862e755b7d7e1e7a01315e7
Luke Amdor <luke@absoluterubbish.net>
2009-10-13 21:01:45 -0500

    Only loading up the color-theme if not in a terminal

--- mine/mine-pretty.el
+++ mine/mine-pretty.el
@@ -1,9 +1,10 @@
 ;; Color theme
-(add-path "site-lisp/color-theme-6.6.0")
-(require 'color-theme)
-(color-theme-initialize)
-(load-file (concat emacs-root "el/color-theme-twilight.el"))
-(color-theme-twilight)
+(when window-system
+  (add-path "site-lisp/color-theme-6.6.0")
+  (require 'color-theme)
+  (color-theme-initialize)
+  (load-file (concat emacs-root "el/color-theme-twilight.el"))
+  (color-theme-twilight))
 
 (global-hl-line-mode t)
 

df6a7aaad7eb6a3c7762a25ddc842ec195ab4b2d
Luke Amdor <luke@absoluterubbish.net>
2009-10-13 21:01:24 -0500

    Added .markdown$ auto-mode for markdown-mode

--- mine/mine-markdown.el
+++ mine/mine-markdown.el
@@ -2,6 +2,7 @@
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.mdwn$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 
 (setq markdown-command "Markdown.pl")
 

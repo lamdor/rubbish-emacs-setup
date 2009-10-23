ff6caa29f4e45adc1e12a09ab1813387d1ffa2b1
Luke Amdor <luke@absoluterubbish.net>
2009-10-23 13:55:25 -0500

    Fixed shell-mode to follow by using dirtrack-mode (I have too many aliases for changing the directory)

--- mine/mine-shell.el
+++ mine/mine-shell.el
@@ -4,4 +4,10 @@
 ;; Start shell or switch to it if it's active.
 (global-set-key (kbd "C-x m") 'shell)
 
+(add-hook 'shell-mode-hook
+          '(lambda ()
+             (setq dirtrack-list '("^\\([^ ]*\\) ?.*" 1 t))
+             (shell-dirtrack-mode -1)
+             (dirtrack-mode t)))
+
 (provide 'mine-shell)

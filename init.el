b9e11a6fd4c41067735f6f905ead53895f468d39
Luke Amdor <luke@absoluterubbish.net>
2009-10-26 10:45:31 -0500

    Opening with the agenda instead of file

--- init.el
+++ init.el
@@ -47,5 +47,5 @@
 
 (message "My .emacs loaded in %ds." (destructuring-bind (hi lo ms) (current-time) (- (+ hi lo) (+ (first *emacs-load-start*) (second *emacs-load-start*)))))
 
-;; Visit GTD file
-(gtd)
+;; Visit GTD Agenda
+(gtd-switch-to-agenda)

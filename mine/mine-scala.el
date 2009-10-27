2ea65cc34327a0d1077369c3805bd6171e91ecde
Luke Amdor <luke@absoluterubbish.net>
2009-10-26 20:32:54 -0500

    Broke out sbt to be its own git repo

--- mine/mine-scala.el
+++ mine/mine-scala.el
@@ -4,11 +4,14 @@
 
 (setq scala-interpreter "/usr/local/bin/scala")
 
-(load-file (concat emacs-root "el/sbt.el"))
+(add-path "site-lisp/sbt")
+(autoload 'sbt "sbt" "sbt shell" t)
+(autoload 'sbt-swith "sbt" "sbt shell switch" t)
 
 (add-hook 'scala-mode-hook
 	  '(lambda ()
-	     (scala-electric-mode t)
+	     (local-set-key (kbd "C-c C-a") 'sbt-switch)
+             (scala-electric-mode t)
              (local-set-key (kbd "RET") 'newline-and-indent)
              (c-subword-mode t)))
 

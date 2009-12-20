9ab7366201e223606d24cf083db414bd72ace071
Luke Amdor <lamdor@geolearning.com>
2009-12-20 09:36:28 -0600

    Adding more ruby files to auto-mode-alist

--- mine/mine-ruby.el
+++ mine/mine-ruby.el
@@ -43,6 +43,8 @@
 ;; File Types
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\config.ru\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 
 ;; Hooks

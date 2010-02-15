28eea14f79597a10904ccecdff246d7c657fc69c
Luke Amdor <lamdor@geolearning.com>
2010-02-15 13:33:24 -0600

    Invoke ruby-mode for lowercase rakefiles

--- mine/mine-ruby.el
+++ mine/mine-ruby.el
@@ -53,7 +53,7 @@
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("config\\.ru\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("[Rr]akefile" . ruby-mode))
 
 ;; Hooks
 (add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))

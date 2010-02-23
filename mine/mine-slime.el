aaf46ddc23915c18c97aa132ce3ab8becdf48f93
Luke Amdor <luke.amdor@gmail.com>
2010-02-22 19:52:55 -0600

    Fixed swank-clojure

--- mine/mine-slime.el
+++ mine/mine-slime.el
@@ -8,9 +8,9 @@
                slime-fuzzy))
 (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode t)))
 
-(add-path "site-lisp/swank-clojure/src/emacs")
+(add-path "site-lisp/swank-clojure")
 (setq swank-clojure-jar-path "/usr/local/Cellar/clojure/1.0.0/clojure-1.0.0.jar") ; needed for swank-clojure to be happy
-(require 'swank-clojure-autoload)
+(require 'swank-clojure)
 
 (defmacro mine-swank-clojure-implementation (name &optional extra-classpath java-path clojure-jar extra-vm-args)
   `(let ((swank-clojure-jar-path ,(or clojure-jar "/usr/local/Cellar/clojure/1.0.0/clojure-1.0.0.jar"))

bd2968ce82307c9da80abda9137130d8f92e0ad7
Luke Amdor <luke.amdor@gmail.com>
2010-05-09 13:13:15 -0500

    Not using a vendored js2 mode, instead using the default/newish javascript mode included in Emacs 23.2

--- mine/mine-javascript.el
+++ mine/mine-javascript.el
@@ -1,6 +1,11 @@
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;; (autoload 'js2-mode "js2" nil t)
+;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;; (setq js2-rebind-eol-bol-keys nil)
 
-(setq js2-rebind-eol-bol-keys nil)
+;; (add-hook 'js-mode '(lambda ())
+;;           (local-set-key (kbd "RET" js-insert-and-indent)))
+
+(setq js-indent-level 2)
+(add-hook 'js-mode-hook 'turn-on-wrap-region-mode)
 
 (provide 'mine-javascript)

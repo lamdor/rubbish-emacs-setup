fbbec1dadbc42b63a86232bc9d13f6f21a92bafe
Luke Amdor <lamdor@geolearning.com>
2010-05-13 10:50:42 -0500

    Using client VM

--- mine/mine-env.el
+++ mine/mine-env.el
@@ -1,5 +1,6 @@
 (setenv "EDITOR" "emacsclient -c")
-(setenv "JAVA_OPTS" "-d32 -Xms256m -Xmx512m")
+(setenv "JAVA_OPTS" "-Xms256m -Xmx512m -XX:MaxPermSize=256m -d32 -client")
+(setenv "MAVEN_OPTS" "-Xmx1024m -Xms256m -XX:MaxPermSize=512m -d32 -client")
 (setenv "ANDROID_SDK_HOME" "/usr/local/Cellar/android-sdk/r5")
 (setenv "ANDROID_SDK_ROOT" "/usr/local/Cellar/android-sdk/r5")
 

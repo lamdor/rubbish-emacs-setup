02261a59c3947fb909768abec1223ddd1ccd1fb5
Luke Amdor <luke.amdor@gmail.com>
2010-08-19 08:00:38 -0500

    Getting more scala things switched over to ensime

--- mine/mine-scala.el
+++ mine/mine-scala.el
@@ -8,41 +8,27 @@
 (autoload 'sbt "sbt" "sbt shell" t)
 (autoload 'sbt-switch "sbt" "sbt shell switch" t)
 
-(autoload 'mvn-keys "mvn" "Sets Maven Keys" t)
-(setq sbt-use-ui t)
-
 (add-path "site-lisp/ensime/dist/elisp")
 (autoload 'ensime-scala-mode-hook "ensime" "ENSIME: The ENhanced Scala Interaction Mode for Emacs (minor-mode)." t)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
-(defun scala-run-scala-sbt ()
-  (interactive)
-  (let ((sbt-path (sbt-find-path-to-project)))
-    (if (equal "/" sbt-path)
-        (scala-run-scala "scala")
-      (progn
-        (cd sbt-path)
-        (scala-run-scala "sbt console")))))
-
+(setq sbt-use-ui t)
 (add-hook 'scala-mode-hook
 	  '(lambda ()
-	     (local-set-key (kbd "C-c C-a") 'sbt-switch)
-             (mvn-keys)
-
-             (scala-electric-mode t)
              (local-set-key (kbd "RET") 'newline-and-indent)
              (c-subword-mode t)
-
              (local-set-key (kbd "C-c C-l") 'scala-load-file)
              (local-set-key (kbd "C-c C-r") 'scala-eval-region)
              (local-set-key (kbd "C-M-x") 'scala-eval-definition)
-             (local-set-key (kbd "C-c C-s") 'scala-run-scala-sbt)
-             (local-set-key (kbd "C-c C-z") 'scala-switch-to-interpreter)
 
              (define-key scala-mode-map (kbd "<f1>") nil) ;; I don't like speedbar popping up
-             ))
+
+             (ensime-scala-mode-hook)
+             ;; ensime does have its own sbt switch, but I just like mine a little more
+             (define-key ensime-mode-map (kbd "C-c C-a") 'sbt-switch)))
 
 (add-hook 'scala-mode-hook 'turn-on-wrap-region-mode)
+
+
 
 (provide 'mine-scala)
 

5c310cbaf9f20a10a5d509be78d4279c00d48869
Luke Amdor <lamdor@geolearning.com>
2010-09-24 13:31:28 -0500

    started work on mine-objc

--- mine/mine-objc.el
+++ mine/mine-objc.el
@@ -29,9 +29,9 @@
 (defun mine-objc-choose-header-mode ()
   (if (string-equal (substring (buffer-file-name) -2) ".h")
       (let ((dot-m-file (concat (substring (buffer-file-name) 0 -1) "m")))
-          (if (file-exists-p dot-m-file)
-              (objc-mode)
-            ))))
+        (if (file-exists-p dot-m-file)
+            (objc-mode)
+          ))))
 (add-hook 'find-file-hook 'mine-objc-choose-header-mode)
 
 (defun mine-objc-toggle-header-and-source nil
@@ -39,16 +39,38 @@
   (interactive)
   (let ((fname buffer-file-name) oname)
     (setq oname
-      (cond
-       ((string-match "\\.h$" fname) (replace-match ".m" nil nil fname))
-       ((string-match "\\.m$" fname) (replace-match ".h" nil nil fname))
-       (t fname)))
+          (cond
+           ((string-match "\\.h$" fname) (replace-match ".m" nil nil fname))
+           ((string-match "\\.m$" fname) (replace-match ".h" nil nil fname))
+           (t fname)))
     (find-file oname)))
+
+(defun xcode:build-and-run ()
+ (interactive)
+ (do-applescript
+  "Tell application \"Xcode\" to Activate
+Tell application \"System Events\"
+     Tell Process \"Xcode\"
+          Key code 36 using (Command down)
+     End Tell
+End Tell"))
+
+(defun xcode:build ()
+ (interactive)
+ (do-applescript
+  "Tell application \"Xcode\" to Activate
+Tell application \"System Events\"
+     Tell Process \"Xcode\"
+          Key code 11 using (Command down)
+     End Tell
+End Tell"))
 
 (add-hook 'objc-mode-hook '(lambda ()
                              (c-set-style "mine-obj-c")
                              (c-subword-mode t)
-                             (local-set-key (kbd "C-x C-v") 'mine-objc-toggle-header-and-source)))
+                             (local-set-key (kbd "C-x C-v") 'mine-objc-toggle-header-and-source)
+                             (local-set-key (kbd "C-c ;") 'xcode:build)
+                             (local-set-key (kbd "C-c '") 'xcode:build-and-run)))
 (add-hook 'objc-mode-hook 'turn-on-wrap-region-mode)
 
 (provide 'mine-objc)

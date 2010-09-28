d1f0fbcd7bb9549e878acf4abf1da8ca2350f494
Luke Amdor <luke.amdor@gmail.com>
2010-09-27 20:54:50 -0500

    Only focusing on Emacs when running window-system

--- mine/mine-macosx.el
+++ mine/mine-macosx.el
@@ -1,7 +1,8 @@
 (setq ns-command-modifier (quote meta))
 
 (add-hook 'server-visit-hook '(lambda ()
-                                (shell-command "osascript -e 'tell application \"Emacs\" to activate' &" nil)
-                                (delete-other-windows)))
+                                (when window-system
+                                 (shell-command "osascript -e 'tell application \"Emacs\" to activate' &" nil)
+                                 (delete-other-windows))))
 
 (provide 'mine-macosx)

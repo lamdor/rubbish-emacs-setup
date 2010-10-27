4141407d1255fd2fe9789b9d78c5f3f77b7d66e2
Luke Amdor <lamdor@geolearning.com>
2010-10-27 12:49:41 -0500

    Kinda revert "Removing slime-implementations and instead using lein-swank to start up a clojure slime session"
    Sometimes, I just want to start up a quick clojure session
    
    This reverts commit 1f1f0f393b2ed03ef997f26d9b3e8e1efcc10f6d.

--- mine/mine-slime.el
+++ mine/mine-slime.el
@@ -9,7 +9,15 @@
 (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode t)))
 
 (add-path "site-lisp/swank-clojure")
-(setq swank-clojure-jar-path "/usr/local/Cellar/clojure/1.0.0/clojure-1.0.0.jar") ; needed for swank-clojure to be happy
 (require 'swank-clojure)
+
+(setq swank-clojure-classpath
+      (list "/usr/local/Cellar/clojure/1.2.0/clojure.jar"
+            (concat emacs-root "site-lisp/swank-clojure/swank-clojure-1.2.0.jar")))
+
+;; slime implementations
+(setq slime-lisp-implementations
+      (list
+       (list 'clojure (swank-clojure-cmd) :init 'swank-clojure-init)))
 
 (provide 'mine-slime)

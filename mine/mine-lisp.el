6bc4d7adaeebdc216c605933b321484a347d8288
Luke Amdor <luke.amdor@gmail.com>
2010-12-26 08:52:39 -0600

    Enabling slime-mode in scheme-mode

--- mine/mine-lisp.el
+++ mine/mine-lisp.el
@@ -1,6 +1,12 @@
 (autoload 'paredit-mode "paredit" "Minor module for psuedo-structually editting lisp code." t)
 
 (add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode t)))
+
+(add-hook 'scheme-mode-hook
+          '(lambda ()
+             (paredit-mode t)
+             (slime-mode t)))
+
 (add-hook 'lisp-mode-hook (lambda () (paredit-mode t)))
 
 (provide 'mine-lisp)

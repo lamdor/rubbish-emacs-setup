b0fca82539929cd926182fa3fcd70da851774517
Luke Amdor <lamdor@geolearning.com>
2010-12-30 10:09:56 -0600

    Removing references to swank-clojure

--- mine/mine-slime.el
+++ mine/mine-slime.el
@@ -8,16 +8,4 @@
                slime-fuzzy))
 (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode t)))
 
-(add-path "site-lisp/swank-clojure")
-(require 'swank-clojure)
-
-(setq swank-clojure-classpath
-      (list "/usr/local/Cellar/clojure/1.2.0/clojure.jar"
-            (concat emacs-root "site-lisp/swank-clojure/swank-clojure-1.2.0.jar")))
-
-;; slime implementations
-(setq slime-lisp-implementations
-      (list
-       (list 'clojure (swank-clojure-cmd) :init 'swank-clojure-init)))
-
 (provide 'mine-slime)

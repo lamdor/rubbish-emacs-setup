44ba6caa4c9a45ee9cb5093a3278af248dff951e
Luke Amdor <lamdor@geolearning.com>
2010-12-30 09:30:36 -0600

    Add eldoc when in emacs-lisp

--- mine/mine-lisp.el
+++ mine/mine-lisp.el
@@ -1,6 +1,9 @@
 (autoload 'paredit-mode "paredit" "Minor module for psuedo-structually editting lisp code." t)
 
-(add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode t)))
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (paredit-mode t)
+            (eldoc-mode t)))
 (add-hook 'lisp-mode-hook (lambda () (paredit-mode t)))
 
 (provide 'mine-lisp)

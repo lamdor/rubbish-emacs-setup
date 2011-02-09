62e5d819db24f256645e2efcb4a2db0d6b052b26
Luke Amdor <luke.amdor@gmail.com>
2011-02-08 21:19:45 -0600

    Using [remap ...] for remappings to inform me later

--- mine/mine-bindings.el
+++ mine/mine-bindings.el
@@ -1,7 +1,7 @@
 (global-set-key (kbd "<f1>") 'ns-toggle-fullscreen)
 
 ;; buffers
-(global-set-key (kbd "C-x C-b") 'bs-show)
+(global-set-key [remap list-buffers] 'bs-show)
 (global-set-key (kbd "C-c y") 'bury-buffer)
 
 ;; frame/window
@@ -11,32 +11,31 @@
 ;; file navigation
 (global-set-key (kbd "C-x C-M-f") 'ack-find-file)
 (global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
-(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
-(global-set-key (kbd "M-.") 'ido-find-tag)
+(global-set-key [remap ido-find-file-read-only] 'ido-recentf-open)
+(global-set-key [remap find-tag] 'ido-find-tag)
 (global-set-key (kbd "C-x C-i") 'ido-imenu)
 
 (global-set-key (kbd "C-c k") 'delete-this-buffer-and-file)
 
 ;; Text Editting
 (global-set-key (kbd "C-x C-k") 'kill-region)
-(global-set-key (kbd "C-a") 'beginning-of-line-or-back-to-indention)
-(global-set-key "\r" 'newline-and-indent)
-(global-set-key (kbd "C-m") 'newline-and-indent)
-(global-set-key (kbd "C-o") 'open-line-and-indent)
-(global-set-key (kbd "C-k") 'kill-to-end-or-join)
+(global-set-key [remap move-to-beginning-of-line] 'beginning-of-line-or-back-to-indention)
+(global-set-key [remap newline] 'newline-and-indent)
+(global-set-key [remap open-line] 'open-line-and-indent)
+(global-set-key [remap kill-line] 'kill-to-end-or-join)
 (global-set-key (kbd "C-c r") 'revert-buffer)
 (global-set-key (kbd "M-g") 'goto-line)
 (global-set-key (kbd "C-c o") 'indent-buffer)
 
 ;; Use regex searches
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
-(global-set-key (kbd "M-%") 'query-replace-regexp)
+(global-set-key [remap isearch-forward] 'isearch-forward-regexp)
+(global-set-key [remap isearch-backward] 'isearch-backward-regexp)
+(global-set-key [remap isearch-forward-regexp] 'isearch-forward)
+(global-set-key [remap isearch-backward-regexp] 'isearch-backward)
+(global-set-key [remap query-replace] 'query-replace-regexp)
 
 ;; smex keybindings
-(global-set-key (kbd "M-x") 'smex)
+(global-set-key [remap execute-extended-command] 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-x C-m") 'smex)
 

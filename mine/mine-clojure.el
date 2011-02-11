d9b9a8569375eadfac9b7d11e5b5d30a0f4d2341
Luke Amdor <lamdor@geolearning.com>
2011-02-11 11:11:21 -0600

    Removing redundant lein-swank command

--- mine/mine-clojure.el
+++ mine/mine-clojure.el
@@ -13,19 +13,4 @@
 ;;(autoload 'clojure-test-maybe-enable "clojure-test-mode" "" t)
 ;;(add-hook 'clojure-mode-hook 'clojure-test-maybe-enable)
 
-(defun lein-swank ()
-  (interactive)
-  (let ((root (locate-dominating-file default-directory "project.clj")))
-    (when (not root)
-      (error "Not in a Leiningen project."))
-    ;; you can customize slime-port using .dir-locals.el
-    (shell-command (format "cd %s && lein swank %s &" root slime-port)
-                   "*lein-swank*")
-    (set-process-filter (get-buffer-process "*lein-swank*")
-                        (lambda (process output)
-                          (when (string-match "Connection opened on" output)
-                            (slime-connect "localhost" slime-port)
-                            (set-process-filter process nil))))
-    (message "Starting swank server...")))
-
 (provide 'mine-clojure)

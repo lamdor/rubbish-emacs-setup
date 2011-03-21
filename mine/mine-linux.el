4a3dbecf8da2d5a9f652b50e9c4d3f0c80b02ed5
Luke Amdor <luke.amdor@t8webware.com>
2011-03-21 11:32:22 -0500

    Enabling copying and paste from emacs to other windows

--- mine/mine-linux.el
+++ mine/mine-linux.el
@@ -1,4 +1,7 @@
 (setq mine-normal-font "Monaco 13")
 (setq mine-big-font "Monaco 20")
 
+(setq x-select-enable-clipboard t)
+(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+
 (provide 'mine-linux)

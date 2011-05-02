37ddfed3236075b6656fde694383441dd0c0deb1
Luke Amdor <luke.amdor@t8webware.com>
2011-05-02 13:45:22 -0500

    don't ask for jira line... yet

--- mine/mine-vc.el
+++ mine/mine-vc.el
@@ -2,4 +2,18 @@
 (add-path "site-lisp/magit/")
 (autoload 'magit-status "magit" "use magit git magic" t)
 
+(setq magit-remote-ref-format 'remote-slash-branch)
+
+;; (defun ask-for-jira-line (line)
+;;   (interactive "sJIRA Line? ")
+;;   line)
+
+;; (add-hook 'magit-log-edit-mode-hook '(lambda ()
+;;                                        (let ((jira-line (call-interactively 'ask-for-jira-line)))
+;;                                          (unless (string= "" jira-line)
+;;                                            (insert "
+
+;; " jira-line)
+;;                                            (goto-char (point-min))))))
+
 (provide 'mine-vc)

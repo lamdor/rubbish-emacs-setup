ab41196aa5c2f6ac7b87071d6187da565b2e5ef0
Luke Amdor <luke.amdor@gmail.com>
2011-08-20 13:54:53 -0500

    updating clojure stuff

--- mine/mine-clojure.el
+++ mine/mine-clojure.el
@@ -2,11 +2,13 @@
 (add-path "site-lisp/clojure-mode")
 (autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
 (add-hook 'clojure-mode-hook (lambda () (paredit-mode t)))
 
 (add-path "site-lisp/elein")
 (autoload 'elein-swank "elein" "Launch lein swank and connect slime to it.  Interactively, aPREFIX means launch a standalone swank session without a project." t)
 (autoload 'elein-run-cmd "elein" "Run 'lein ARGS' using `compile' in the project root directory." t)
+
 
 ;; clojure-test-mode
 ;;(autoload 'clojure-test-mode "clojure-test-mode" "Clojure test mode" t)

d5b0a5bcacda0e89f13e67e392f477905e6e41f7
Luke Amdor <luke.amdor@t8webware.com>
2011-12-28 08:26:44 -0600

    fixing JAVA_OPTS and slime so clojure swank works

--- mine/mine-env.el
+++ mine/mine-env.el
@@ -1,6 +1,6 @@
 (setenv "EDITOR" "emacsclient -c")
-(setenv "JAVA_OPTS" "-Xms256m -Xmx512m -XX:MaxPermSize=256m -client")
-(setenv "MAVEN_OPTS" "-Xmx1024m -Xms256m -XX:MaxPermSize=512m -client")
+(setenv "JAVA_OPTS" "-Xmx512m")
+(setenv "MAVEN_OPTS" "-Xmx1024m -Xms256m -XX:MaxPermSize=512m")
 (setenv "ANDROID_SDK_HOME" "/usr/local/Cellar/android-sdk/r5")
 (setenv "ANDROID_SDK_ROOT" "/usr/local/Cellar/android-sdk/r5")
 

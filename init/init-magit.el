c57e1c4535d36a58b177931a77a07086dd21f266
Luke Amdor <luke.amdor@t8webware.com>
2012-07-11 15:27:59 -0500

    making prettier commit messages

--- init/init-magit.el
+++ init/init-magit.el
@@ -2,3 +2,11 @@
 (setq magit-remote-ref-format 'remote-slash-branch)
 
 (global-set-key (kbd "C-M-g") 'magit-status)
+
+(add-hook 'magit-log-edit-mode-hook '(lambda () (flyspell-mode t)))
+
+(add-hook 'magit-log-edit-mode-hook
+          '(lambda ()
+             (set (make-variable-buffer-local 'whitespace-style) '(face lines-tail))
+             (set (make-variable-buffer-local 'whitespace-line-column) 72)
+             (whitespace-mode t)))

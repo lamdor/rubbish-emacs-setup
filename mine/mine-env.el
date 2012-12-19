4c788d911609d8e1773967439a88afa73c436f3c
Luke Amdor <luke.amdor@gmail.com>
2012-12-18 22:52:03 -0600

    setting only path

--- mine/mine-env.el
+++ mine/mine-env.el
@@ -1,8 +1,9 @@
+(setenv "PATH" (concat (getenv "HOME") "/bin" ":"
+                       "/usr/local/bin" ":"
+                       "/usr/bin" ":"
+                       "/bin" ":"
+                       "/sbin" ":"))
+
 (setenv "EDITOR" "emacsclient")
-;; (setenv "JAVA_OPTS" "-Xmx512m")
-(setenv "MAVEN_OPTS" "-Xmx1024m -Xms256m -XX:MaxPermSize=512m")
-(setenv "ANDROID_SDK_HOME" "/usr/local/Cellar/android-sdk/r5")
-(setenv "ANDROID_SDK_ROOT" "/usr/local/Cellar/android-sdk/r5")
-(setenv "CLOJURESCRIPT_HOME" (concat (getenv "HOME") "/code/watch/clojurescript"))
 
 (provide 'mine-env)

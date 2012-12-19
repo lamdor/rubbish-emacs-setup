49f91eaf0e8b113f40047f3bfad7a5c849d0ea6a
Luke Amdor <luke.amdor@gmail.com>
2012-12-19 09:06:46 -0600

    only run custom/*.el if custom dir exists

--- init.el
+++ init.el
@@ -17,8 +17,10 @@
   ('gnu/linux (require 'mine-linux)))
 
 ;; load files under custom/*.el
-(let ((custom-files (directory-files "~/.emacs.d/custom/" t "\.el$")))
-  (mapcar 'load-file custom-files))
+(setq mine-custom-dir "~/.emacs.d/custom/")
+(if (file-exists-p mine-custom-dir)
+ (let ((custom-files (directory-files mine-custom-dir t "\.el$")))
+  (mapcar 'load-file custom-files)))
 
 (setq custom-file (expand-file-name "~/.emacs.d/customizations.el"))
 (load custom-file)

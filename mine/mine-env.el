5b501a0900c430d2a000944955a7bbc53928bffc
Luke Amdor <luke.amdor@gmail.com>
2012-12-20 10:40:50 -0600

    setting the exec-path

--- mine/mine-env.el
+++ mine/mine-env.el
@@ -4,6 +4,12 @@
                        "/bin" ":"
                        "/sbin" ":"))
 
+(setq exec-path (list (concat (getenv "HOME") "/bin")
+                      "/usr/local/bin"
+                      "/usr/bin"
+                      "/bin"
+                      "/sbin"))
+
 (setenv "EDITOR" "emacsclient")
 
 (provide 'mine-env)

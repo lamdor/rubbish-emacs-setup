0886b967b134e9e795cc42e5f816eac049f31a85
Luke Amdor <luke.amdor@gmail.com>
2012-12-25 15:09:18 -0600

    raising emacs if an emacs frame is created

--- mine/mine-macosx.el
+++ mine/mine-macosx.el
@@ -1,4 +1,11 @@
-(global-set-key (kbd "<f1>") 'ns-toggle-fullscreen)
 (setq ns-command-modifier (quote meta))
+
+(defun ns-raise-emacs ()
+  (ns-do-applescript "tell application \"Emacs\" to activate"))
+
+(add-to-list 'after-make-frame-functions
+             '(lambda (f)
+                (if (display-graphic-p f) ;; is a graphical frame
+                    (ns-raise-emacs))))
 
 (provide 'mine-macosx)

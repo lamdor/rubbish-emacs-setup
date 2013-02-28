0cc9891c716453d9ff609df99af4ef3d91d9717a
Luke Amdor <luke.amdor@gmail.com>
2013-02-28 10:31:05 -0600

    fixing paredit diminish

--- init/init-paredit.el
+++ init/init-paredit.el
@@ -1,3 +1,3 @@
 (add-hook 'lisp-mode-hook (lambda () (paredit-mode t)))
 (add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode t)))
-(diminish 'paredit-mode)
+(eval-after-load "paredit" '(diminish 'paredit-mode))

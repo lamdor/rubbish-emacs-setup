b42058d9029220ea126a4bdac7cfbef0967ec51d
Luke Amdor <luke.amdor@gmail.com>
2013-03-20 13:44:45 -0500

    removing redundant toggle-fullscreen func

--- mine/mine-defuns.el
+++ mine/mine-defuns.el
@@ -236,15 +236,6 @@ frames with exactly two windows."
   (delete-region (point-min) (point-max))
   (comint-send-input))
 
-(defun toggle-fullscreen (&optional f)
-  (interactive)
-  (let ((current-value (frame-parameter nil 'fullscreen)))
-    (set-frame-parameter nil 'fullscreen
-                         (if (equal 'fullboth current-value)
-                             (if (boundp 'old-fullscreen) old-fullscreen nil)
-                           (progn (setq old-fullscreen current-value)
-                                  'fullboth)))))
-
 (defun dired-ediff-marked-files ()
   "Run ediff on marked ediff files."
   (interactive)

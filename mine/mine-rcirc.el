4d9bfeaf2667f9ee4db430ef9c08e6d95fa57563
Luke Amdor <luke.amdor@gmail.com>
2013-05-03 09:46:05 -0500

    adding mine-rcirc-shut-up func

--- mine/mine-rcirc.el
+++ mine/mine-rcirc.el
@@ -2,6 +2,11 @@
 (add-hook 'rcirc-mode-hook (lambda () (rcirc-track-minor-mode t)))
 (add-hook 'rcirc-mode-hook (lambda () (rcirc-omit-mode)))
 
+(defun mine-rcirc-shut-up ()
+  (interactive)
+  (rcirc-track-minor-mode -1)
+  (remq 'rcirc-activity-string global-mode-string))
+
 (setq rcirc-fill-flag nil
       rcirc-notify-timeout 2
       rcirc-notify-message "%s: %s"

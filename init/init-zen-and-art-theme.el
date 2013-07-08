14cf0cdd982ded736edec53426b7d3db60e3733e
Luke Amdor <luke.amdor@gmail.com>
2013-07-08 14:27:44 -0500

    adding function for resetting modeline

--- init/init-zen-and-art-theme.el
+++ init/init-zen-and-art-theme.el
@@ -1,2 +1,6 @@
 (setq frame-background-mode 'dark)
 (load-theme 'zen-and-art t)
+
+(defun reset-zen-and-art-mode-line-colors ()
+  (interactive)
+  (set-face-attribute 'mode-line nil :background "grey40" :foreground "white"))

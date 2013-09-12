0b3130808662c6dc552c19b15a6d920f7cb9c72b
Luke Amdor <luke.amdor@gmail.com>
2013-09-12 11:52:38 -0500

    removing old dir

--- old/mine/mine-erlang.el
+++
@@ -1,7 +0,0 @@
-;; Erlang Support
-(add-path "site-lisp/erlang/")
-(setq erlang-root-dir "/usr/local/lib/erlang")
-(setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
-(require 'erlang-start)
-
-(provide 'mine-erlang)
--- old/mine/mine-pomodoro.el
+++
@@ -1,9 +0,0 @@
-(require 'pomodoro)
-
-(add-hook 'pomodoro-finished-hook '(lambda ()
-                                     (growl-message "Pomodoro Finished" 2)))
-
-(global-set-key (kbd "C-c p s") 'pomodoro-start)
-(global-set-key (kbd "C-c p v") 'pomodoro-void)
-
-(provide 'mine-pomodoro)

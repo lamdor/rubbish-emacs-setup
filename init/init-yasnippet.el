298d1aa973a29e55e06a112c9294930571d9125a
Luke Amdor <luke.amdor@gmail.com>
2013-11-07 20:46:00 -0600

    fixing yas dirs

--- init/init-yasnippet.el
+++ init/init-yasnippet.el
@@ -2,4 +2,6 @@
 
 (diminish 'yas-minor-mode)
 
+(setq yas-snippet-dirs (remove "~/.emacs.d/snippets" yas-snippet-dirs))
 (add-to-list 'yas-snippet-dirs "~/.emacs.d/custom/snippets")
+(yas/reload-all)

e253062fe6ec16b8cefa6bb085c197932c230c2b
Luke Amdor <luke.amdor@gmail.com>
2013-11-12 20:50:26 -0600

    adding handle string-trim

--- mine/mine-defuns.el
+++ mine/mine-defuns.el
@@ -301,4 +301,9 @@ frames with exactly two windows."
   (interactive)
   (shell-command "uuidgen | tr -d '\n' | tr '[A-Z]' '[a-z]'" t))
 
+(defun string-trim (string)
+  "Remove white spaces in beginning and ending of STRING.
+White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
+  (replace-regexp-in-string "\\`[ \t\n]*" "" (replace-regexp-in-string "[ \t\n]*\\'" "" string)))
+
 (provide 'mine-defuns)

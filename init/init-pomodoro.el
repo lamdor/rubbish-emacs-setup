031311c1be3c67ba7e6ddb8d3f04c12bad3c6acc
Luke Amdor <luke.amdor@gmail.com>
2014-03-10 22:02:24 -0500

    no rcirc anymore

--- init/init-pomodoro.el
+++ init/init-pomodoro.el
@@ -1,6 +1,5 @@
 (global-set-key [f8] 'pomodoro-start-or-void)
 
-(add-hook 'pomodoro-start-hook 'mine-rcirc-shut-up)
 (add-hook 'pomodoro-void-hook 'rcirc-track-minor-mode)
 (add-hook 'pomodoro-finished-hook 'rcirc-track-minor-mode)
 

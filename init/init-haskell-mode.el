05ce564f946bd1dfbca96907c25f95d042093187
Luke Amdor <luke.amdor@gmail.com>
2014-05-17 13:52:18 -0500

    fixing haskell ghci to include src dir

--- init/init-haskell-mode.el
+++ init/init-haskell-mode.el
@@ -5,3 +5,9 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 
+(setq haskell-program-name "ghci -isrc")
+
+;; (eval-after-load "haskell-mode"
+;;     '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
+;; (eval-after-load "haskell-cabal"
+;;     '(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile))

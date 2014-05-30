986bc8ad17a64cffd44bfe0c26ccf031e1aea7b0
Luke Amdor <luke.amdor@gmail.com>
2014-05-30 08:23:57 -0500

    customizations.el update

--- customizations.el
+++ customizations.el
@@ -5,19 +5,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "5e1d1564b6a2435a2054aa345e81c89539a72c4cad8536cfe02583e0b7d5e2fa" default)))
+ '(magit-diff-options nil)
  '(safe-local-variable-values (quote ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eshell-prompt ((t (:foreground "Purple" :bold t))))
  '(org-agenda-date ((t (:inherit font-lock-type-face))) t)
  '(org-agenda-date-weekend ((t (:inherit org-agenda-date))) t)
+ '(org-agenda-done ((t (:inherit org-archived :strike-through t))))
  '(org-column ((t (:background "#222222"))))
  '(org-column-title ((t (:background "DarkGreen" :foreground "white" :bold t :box (:line-width 1 :style released-button)))))
  '(org-date ((t (:inherit font-lock-constant-face))))
- '(org-done ((t (:background "DarkGreen" :foreground "white" :box (:line-width 1 :style released-button)))))
+ '(org-done ((t (:background "DarkGreen" :foreground "white" :strike-through t :box (:line-width 1 :style released-button)))))
+ '(org-headline-done ((t (:inherit org-archived :strike-through t))))
  '(org-hide ((t (:foreground "#191919"))))
  '(org-level-1 ((t (:inherit outline-1))))
  '(org-level-2 ((t (:inherit outline-2))))

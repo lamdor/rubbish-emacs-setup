50dd6f787e26b5aa1abe87b59d2d5fd734caab86
Luke Amdor <luke.amdor@gmail.com>
2014-06-18 09:03:14 -0500

    adding all the haskell config

--- mine/mine-pkgmgt.el
+++ mine/mine-pkgmgt.el
@@ -167,10 +167,26 @@
   (progn
     (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
     (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
-    (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-    ;; (diminish 'haskell-indentation-mode)
-    ;; (diminish 'haskell-doc-mode)
-    ))
+    (add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
+
+    (setq haskell-process-suggest-remove-import-lines t
+          haskell-process-auto-import-loaded-modules t
+          haskell-process-suggest-hoogle-imports nil ;; 'cabal install hoogle' fails
+          haskell-process-log t
+          haskell-process-type 'cabal-repl
+          haskell-interactive-mode-eval-mode 'haskell-mode
+          haskell-process-show-debug-tips nil)
+    
+    (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+    (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-bring)
+    (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+    (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+    (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+    (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+    (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+    (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def-or-tag)
+    (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+    (eval-after-load 'haskell-cabal '(define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal))))
 
 (use-package ruby-mode
   :mode (("Vagrantfile$" . ruby-mode)
